c3610ac808e528c1ec02c389ed9f18aa4e010b03
Lorren.Wang <xianxiuo@163.com>
2015-11-05 16:52:58 +0800

    Change-Id: Ied36e510dd45a50d0aa7d9d2ba6a06ef4a638bda Signed-off-by: Lorren.Wang <xianxiuo@163.com>

---
+++ src/main/java/com/lorren/util/ConstantValue.java
@@ -0,0 +1,14 @@
+package com.lorren.util;
+
+public class ConstantValue {
+    
+    private ConstantValue() {}
+    
+    public static final String VALUE_ALL = "ALL";
+    
+    public static final String PARAM_ID = "id";
+    
+    public static final String TABLE_ACCOUNT = "tb_account";
+    public static final String TABLE_USER = "tb_user";
+    
+}
